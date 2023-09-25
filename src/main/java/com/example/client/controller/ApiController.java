3550a2520561781e5568c48d5c8dd33c702c7a9c
parfait2 <parkyl0208@naver.com>
2023-09-25 17:31:56 +0900

    [Add] client first commit

---
+++ src/main/java/com/example/client/controller/ApiController.java
@@ -0,0 +1,23 @@
+package com.example.client.controller;
+
+import com.example.client.service.RestTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ApiController {
+    // @Autowired는 예전에 사용하던 방식으로 요즘에는 생성자 주입 방식으로 바뀜.(명시적)
+    private final RestTemplateService restTemplateService;
+
+    public ApiController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping("")
+    public String getHello() {
+        return restTemplateService.hello();
+    }
+}
